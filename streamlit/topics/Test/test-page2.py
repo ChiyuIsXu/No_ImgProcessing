5804c7b20ca06f06489ae9da8fac4462045e5631
NoVarYe <novarye@qq.com>
2024-08-16 15:18:03 +0800

    Finish UI Framework

---
+++ streamlit/topics/Test/test-page2.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+import tomllib as tl
+import pandas as pd
+import streamlit as st
+import seaborn as sns
+import plotly.express as px
+
+# Initialize the App
+if "current_page" not in st.session_state:
+    st.session_state.current_page = None
+current_page = st.session_state.get("current_page", None)
+
+# Sidebar
+
+
+# st.sidebar.title("Test")
+
+st.title("Test 2")
+
+st.write(f"Current Page: {current_page}")
